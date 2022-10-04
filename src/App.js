e22bbeeedb1f342e88cee84929fc85da73320f53
zoeruo <dodoruo@gmail.com>
2022-10-04 00:06:23 -0400

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import '../dist/output.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="color-change">
+        <p className="color-change text-3xl font-bold underline">
           Edit <code>src/App.js</code> and save to reload.<br>
           </br>
           CSS style
