2c334fb4287ad8016163f9a390c5430c955372ec
zoeruo <dodoruo@gmail.com>
2022-10-03 22:46:06 -0400

    js changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="color-change">
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.<br>
+          </br>
+          CSS style not working.
         </p>
         <a
           className="App-link"
